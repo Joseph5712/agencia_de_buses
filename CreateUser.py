5e3093c2eeb22f5ce0076e2093b74d9c216f764e
Luis Auz <luisauz.geek@gmail.com>
2021-09-27 11:01:35 -0500

    add: Users Methods

---
+++ CreateUser.py
@@ -0,0 +1,3 @@
+class CreateUser:
+    def __init__(self) -> None:
+        pass
--- User.py
+++ User.py
@@ -1,6 +1,16 @@
+from typing import final
+import psycopg2
+
 class User:
 
     IS_ADMIN:bool = False
+    CONNECTION = psycopg2.connect(
+                        user='postgres',
+                        password='postgres',
+                        host='127.0.0.1',
+                        port=5432,
+                        database='agencia_buses'
+                        )
 
     def __init__(self, name:str, lastname:str, username:str, password:str) -> object:
         self._name:str = name
@@ -29,3 +39,47 @@ class User:
         user = self.username
         password = self.password
         return [user,password]
+
+    def createUser(self):
+        user = self.username
+        password = self.password
+        name = self.name
+        lastname = self.lastname
+        connection = self.CONNECTION
+        try:
+            with connection:
+                with connection.cursor() as cursor:
+                    sentence:str = 'INSERT INTO public."Users"(username,password,name_user,last_name_user,admin) VALUES(%s,%s,%s,%s,%s)'
+                    cursor.execute(sentence,(user,password,name,lastname,self.IS_ADMIN))
+                    return True
+        except Exception as e:
+            print(f"Error happened = {e}")
+            return False
+        finally:
+            connection.close()
+    
+    def successCreation(self):
+        createUser = self.createUser()
+        if createUser == True:
+            return True
+        else:
+            return False
+    
+    def isAdmin(self):
+        user = self.username
+        connection = self.CONNECTION
+        try:
+            with connection:
+                with connection.cursor() as cursor:
+                    sentence:str = 'SELECT admin from public."Users" WHERE username=%s'
+                    cursor.execute(sentence,(user,))
+                    result:bool = cursor.fetchone()[0]
+                    return result
+        except Exception as e:
+            print(f"Error happened: {e}")
+        finally:
+            connection.close()
+
+if __name__ == '__main__':
+    userNew:User = User('Marjorie','Garcia','1304140195','1234567890')
+    print(userNew.isAdmin())
