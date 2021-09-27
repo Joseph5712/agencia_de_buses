80a9e148a1fd1a876a5203f9f9dc711662167082
Luis Antonio Auz Garcia <paxauz@pop-os.localdomain>
2021-09-27 08:51:11 -0500

    del: All old version

--- constantes.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-# Lista de constantes
-adminRole = 'admin'
-role = 'pasajero'
-db = sql.connect('buses_system.db')
-lugares = ["San José","Alajuela","Heredia","Cartago","San Carlos","Puntarenas","Limón"]
-placaLetras ="QWRTYPSDFGHJKLZXCVBNM"
-placaNumbers = "0123456879"
