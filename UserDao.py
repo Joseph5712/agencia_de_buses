3f15143a735b3ee43d0aa25365cf67710681ce96
Luis Auz <luisauz.geek@gmail.com>
2021-10-08 21:01:47 -0500

    feat: (finished) UserDao Class

--- UserDao.py
+++ UserDao.py
@@ -5,6 +5,30 @@ from CursorPool import CursorPool
 class UserDao:
     _SELECT:str = "SELECT * FROM users"
     _INSERT:str = "INSERT INTO users(username,password,names,lastname,is_admin) VALUES(%s,%s,%s,%s,%s)"
+    _UPDATE:str = "UPDATE users SET username=%s,password=%s,names=%s,lastname=%s WHERE id_user=%s"
+    _DELETE:str = "DELETE FROM users WHERE id_user=%s"
+
+    @classmethod
+    def deleteUser(cls,user:User):
+        try:
+            with CursorPool() as cursor:
+                values:tuple = (user.idUser,)
+                cursor.execute(cls._DELETE,values)
+                log.debug(f"User deleted: {user}")
+                return cursor.rowcount
+        except Exception as e:
+            log.error(f"Error happened while deleting user: {e}")
+
+    @classmethod
+    def updateUser(cls,user:User):
+        try:
+            with CursorPool() as cursor:
+                values:tuple = (user.username,user.password,user.name,user.lastName,user.idUser)
+                cursor.execute(cls._UPDATE,values)
+                log.debug(f"User updated: {user}")
+                return cursor.rowcount
+        except Exception as e:
+            log.error(f"Error happened while updating user: {e}")
 
     @classmethod
     def getAllUsers(cls):
@@ -39,6 +63,9 @@ class UserDao:
             log.error(f"Error happened while inserting user: {e}")
 
 if __name__ == "__main__":
-    # adminUser:User = User(username="1315490969",password="123456",name="Luis",lastName="Auz",admin=True)
-    # UserDao.insertUser(adminUser)
-    # UserDao.getAllUsers()
+    # user1:User = User(username="1315490977",password="789123",name="Gabriel",lastName="Auz",admin=False,idUser=2)
+    user1:User = User(username="9999999999",password="9999999999",name="User",lastName="Test",idUser=3)
+    # UserDao.insertUser(user1)
+    # UserDao.updateUser(user1)
+    UserDao.deleteUser(user1)
+    UserDao.getAllUsers()
