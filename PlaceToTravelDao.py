0914ef3d0ee60d21efb353941d05a93d60db672b
Luis Auz <luisauz.geek@gmail.com>
2021-10-08 21:23:41 -0500

    feat: (finished) PlaceToTravelDao Class

--- PlaceToTravelDao.py
+++ PlaceToTravelDao.py
@@ -1,4 +1,3 @@
-from os import name
 from logger_base import log
 from CursorPool import CursorPool
 from PlaceToTravel import PlaceToTravel
@@ -6,6 +5,30 @@ from PlaceToTravel import PlaceToTravel
 class PlaceToTravelDao:
     _SELECT:str = "SELECT * FROM places"
     _INSERT:str = "INSERT INTO places(name) VALUES(%s)"
+    _UPDATE:str = "UPDATE places SET name=%s WHERE id_place=%s"
+    _DELETE:str = "DELETE FROM places WHERE id_place=%s"
+
+    @classmethod
+    def deletePlace(cls,place:PlaceToTravel):
+        try:
+            with CursorPool() as cursor:
+                values:tuple = (place.idPlace,)
+                cursor.execute(cls._DELETE,values)
+                log.debug(f"Place deleted: {place}")
+                cursor.rowcount
+        except Exception as e:
+            log.error(f"Error happened while deleting place: {e}")
+
+    @classmethod
+    def updatePlace(cls,place:PlaceToTravel):
+        try:
+            with CursorPool() as cursor:
+                values:tuple = (place.namePlace,place.idPlace)
+                cursor.execute(cls._UPDATE,values)
+                log.debug(f"Place updated: {place}")
+                cursor.rowcount
+        except Exception as e:
+            log.error(f"Error happened while updating place: {e}")
 
     @classmethod
     def getAllPlaces(cls):
@@ -43,8 +66,12 @@ if __name__ == "__main__":
     place3:PlaceToTravel = PlaceToTravel(namePlace="Cartago")
     place4:PlaceToTravel = PlaceToTravel(namePlace="San Carlos")
     place5:PlaceToTravel = PlaceToTravel(namePlace="Puntarenas")
-    place6:PlaceToTravel = PlaceToTravel(namePlace="Limón")
+    place6:PlaceToTravel = PlaceToTravel(namePlace="Limón",idPlace=7)
+    place7:PlaceToTravel = PlaceToTravel(namePlace="Testing Place",idPlace=8)
     places:list = [place1,place2,place3,place4,place5,place6]
-    for place in places:
-        PlaceToTravelDao.insertPlace(place)
+    # for place in places:
+        # PlaceToTravelDao.insertPlace(place)
+    # PlaceToTravelDao.updatePlace(place6)
+    # PlaceToTravelDao.insertPlace(place7)
+    PlaceToTravelDao.deletePlace(place7)
     PlaceToTravelDao.getAllPlaces()
