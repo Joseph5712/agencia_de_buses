ff7942010e1c6b1fa67fa7dce2364942f60da079
Luis Auz <luisauz.geek@gmail.com>
2021-10-12 17:45:01 -0500

    feat: login function working on Menu System

--- Menu.py
+++ Menu.py
@@ -4,11 +4,18 @@ TITLE_START:str = "AUTOBUS SYSTEM"
 start_message:str = f"""{TITLE_START.center(50,"-")}\n1) Login\n2) Exit\nInsert your option: """
 option:int = int(input(start_message))
 if option == 1:
-    username:str = input("Write your username: ")
-    password:str = input("Write your password: ")
-    login:Login = Login(username,password)
-    isAdmin:bool = login.login
-    if isAdmin:
-        print(f"The user {login.username} is Admin")
-    else:
-        print(f"The user {login.username} is a normal user")
+    login_user = None
+    while type(login_user) != bool:
+        username:str = input("Write your username: ")
+        password:str = input("Write your password: ")
+        login:Login = Login(username,password)
+        login_user:bool or int= login.login()
+        if login_user == Login._STATUS_USERNAME_FAILED:
+            print("Username Incorrect")
+        elif login_user == Login._STATUS_PASSWORD_FAILED:
+            print(f"Password incorrect to user {login.username}")
+        else:
+            if login_user == True:
+                print(f"The user {login.username} is Admin")
+            else:
+                print(f"The user {login.username} is a normal user")
