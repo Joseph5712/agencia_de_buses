70e21aed9b8b094e54934863e558ee80108827e5
Luis Auz <luisauz.geek@gmail.com>
2021-10-12 17:43:57 -0500

    feat: Add validations into user & password checking functions.

--- Login.py
+++ Login.py
@@ -3,7 +3,12 @@ from logger_base import log
 from User import User
 
 class Login:
-
+    _STATUS_OK:int = 1
+    _STATUS_FAILED:int = 0
+    _STATUS_USERNAME_OK:int = 2
+    _STATUS_USERNAME_FAILED:int = -2
+    _STATUS_PASSWORD_OK:int = 3
+    _STATUS_PASSWORD_FAILED:int = -3
     _LOGIN_USERNAME:str = "SELECT * FROM users WHERE username=%s"
     _LOGIN:str = "SELECT * FROM users WHERE username=%s AND password=%s"
 
@@ -21,17 +26,18 @@ class Login:
 
     def validationUsername(self,result:tuple):
         if result!=None:
-            return 1
+            return 2
         else:
             log.error(f"Username is not correct")
-            return 0
+            return -2
     
     def validationPassword(self,result:tuple):
         if result!=None:
             log.debug(f"User {self.username} logged succesfully")
-            return 1
+            return 3
         else:
             log.error(f"Password incorrect to user {self.username}")
+            return -3
 
     def login(self):
         try:
@@ -40,24 +46,31 @@ class Login:
                 cursor.execute(self._LOGIN_USERNAME,values)
                 result_login_username:tuple = cursor.fetchone()
                 log.debug(result_login_username)
-                if self.validationUsername(result_login_username) == 1:
+                validateUser = self.validationUsername(result=result_login_username)
+                if validateUser == self._STATUS_USERNAME_OK:
                     newValues:tuple = (self.username,self.password)
                     cursor.execute(self._LOGIN,newValues)
                     result_login:tuple = cursor.fetchone()
-                    user:User = User(
-                        result_login[0],
-                        result_login[1],
-                        result_login[2],
-                        result_login[3],
-                        result_login[4],
-                        result_login[5])
-                    isAdmin:bool = user.isAdmin
-                    return isAdmin
+                    validatePassword = self.validationPassword(result=result_login)
+                    if validatePassword == self._STATUS_PASSWORD_OK:
+                        user:User = User(
+                            result_login[0],
+                            result_login[1],
+                            result_login[2],
+                            result_login[3],
+                            result_login[4],
+                            result_login[5])
+                        isAdmin:bool = user.isAdmin
+                        return isAdmin
+                    else:
+                        return self._STATUS_PASSWORD_FAILED
+                else:
+                    return self._STATUS_USERNAME_FAILED
         except Exception as e:
             log.error(f"Error happened while login user {self.username}: {e}")
 
 if __name__ == "__main__":
-    login:Login = Login("1315490969","12345")
+    login:Login = Login("131","12345")
     isAdmin:bool = Login.login(login)
     if isAdmin:
         log.debug(f"The user {login.username} is Admin")
