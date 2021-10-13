4fc1d69bda77f7383590e3cbcd171291dedfc6f6
Luis Auz <luisauz.geek@gmail.com>
2021-10-13 11:57:14 -0500

    feat: options to CRUD Terminal (whitouth validations)

--- Menu.py
+++ Menu.py
@@ -1,7 +1,16 @@
 from Login import Login
+from Terminal import Terminal
+from TerminalDao import TerminalDao
+from PlaceToTravelDao import PlaceToTravelDao
 
 TITLE_START:str = "AUTOBUS SYSTEM"
+MENU_ADMIN:str = "ADMIN MENU"
+MENU_TERMINAL:str = "MANAGE TERMINAL"
 start_message:str = f"""{TITLE_START.center(50,"-")}\n1) Login\n2) Exit\nInsert your option: """
+menuAdmin:str = f"""{MENU_ADMIN.center(50,"-")}\n1) Manage Terminal\n2) Manage Units Bus\n3) Manage Routes\n4) Reports\n5) Exit\nInsert your option: """
+menuTerminal:str = f"""{MENU_TERMINAL.center(50,"-")}\n1) Get All Terminals\n2) Create Terminal\n3) Update Terminal\n4) Delete Terminal\n5) Back\n6) Exit Program\nInsert your option: """
+
+
 option:int = int(input(start_message))
 if option == 1:
     login_user = None
@@ -16,6 +25,32 @@ if option == 1:
             print(f"Password incorrect to user {login.username}")
         else:
             if login_user == True:
-                print(f"The user {login.username} is Admin")
+                adminOption:int = int(input(menuAdmin))
+                if adminOption == 1:
+                    terminalOption:int = int(input(menuTerminal))
+                    if terminalOption == 1:
+                        TerminalDao.getAllTerminals()
+                    if terminalOption == 2:
+                        nameTerminal:str = input("Write Terminal Name: ")
+                        PlaceToTravelDao.getAllPlaces()
+                        placeTerminal:int = int(input("Write id to Place: "))
+                        terminal:Terminal = Terminal(name=nameTerminal,place=placeTerminal)
+                        TerminalDao.insertTerminal(terminal)
+                    if terminalOption == 3:
+                        TerminalDao.getAllTerminals()
+                        idTerminal:int = int(input("Write id terminal to update: "))
+                        nameTerminal:str = input("Write Terminal Name: ")
+                        PlaceToTravelDao.getAllPlaces()
+                        placeTerminal:int = int(input("Write id to Place: "))
+                        terminal:Terminal = Terminal(
+                            idTerminal=idTerminal,
+                            name=nameTerminal,
+                            place=placeTerminal)
+                        TerminalDao.updateTerminal(terminal)
+                    if terminalOption == 4:
+                        TerminalDao.getAllTerminals()
+                        idTerminal:int = int(input("Write id terminal to delete: "))
+                        terminal:Terminal = Terminal(idTerminal=idTerminal)
+                        TerminalDao.deleteTerminal(terminal)
             else:
                 print(f"The user {login.username} is a normal user")
